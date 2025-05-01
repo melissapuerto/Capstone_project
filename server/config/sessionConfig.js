0efd77b17ff382aa7c03ee9e6f103d22adbb87ca
Tasluf665 <tasluf665@gmail.com>
2025-05-01 04:12:49 +0300

    cc

--- server/config/sessionConfig.js
+++ server/config/sessionConfig.js
@@ -2,9 +2,4 @@ module.exports = {
     secret: 'secret',
     resave: false,
     saveUninitialized: true,
-    cookie: {
-        secure: true,        // Ensure cookies are sent over HTTPS
-        httpOnly: true,      // Prevent client-side JavaScript from accessing cookies
-        sameSite: 'none',    // Allow cookies to be sent across different origins
-    },
 };
