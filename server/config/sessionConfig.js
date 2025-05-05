fdbfbf8860d36843d269da113aa4af5152b477a3
Tasluf665 <tasluf665@gmail.com>
2025-05-05 18:36:59 +0200

    MongoStore added

--- server/config/sessionConfig.js
+++ server/config/sessionConfig.js
@@ -1,7 +1,13 @@
+const MongoStore = require('connect-mongo');
+
 module.exports = {
     secret: 'secret',
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        collectionName: 'sessions'
+    }),
     cookie: {
         secure: true,        // Ensure cookies are sent over HTTPS
         httpOnly: true,      // Prevent client-side JavaScript from accessing cookies
